62b3c4a74df5cd9d49db3768bc3a23e632b7d5d6
iloster <243849707@qq.com>
2016-08-03 23:14:01 +0800

    =

---
+++ start.sh
@@ -0,0 +1,6 @@
+export LC_APP_ID="Jp1kDCMo0VxKSvMH5gUWWQqk-gzGzoHsz"
+export LC_APP_KEY="B8hqfXV5YzF5tkfWwO5tDVMC"
+export LC_APP_MASTER_KEY="Jg4djVTWInzQruxT7zGJNwvx"
+export LC_APP_PORT=3000
+
+python wsgi.py
