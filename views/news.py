eb053570daa5f5cdcdb1b9c541c678ec00b16cce
iloster <243849707@qq.com>
2016-08-04 23:27:13 +0800

    =

--- views/news.py
+++ views/news.py
@@ -9,6 +9,7 @@ from flask import redirect
 from flask import url_for
 from flask import render_template
 import urllib2
+import json
 
 # class News(Object):
 #     pass
@@ -16,8 +17,19 @@ import urllib2
 news_view = Blueprint('news', __name__)
 
 
-@news_view.route('')
-def show():
-	html = urllib2.urlopen("http://c.m.163.com/nc/topicset/ios/subscribe/manage/listspecial.html").read()
-	return html
 
+@news_view.route('')
+def getTopicSet():
+	topicSet = urllib2.urlopen("http://c.m.163.com/nc/topicset/ios/subscribe/manage/listspecial.html").read()
+	return topicset
+
+@news_view.route('/type/<newsType>/list/<int:page>')
+def getListByPage(newsType,page):
+	# urlPrefix = "http://c.m.163.com/nc/article/list/";
+	f = file("config/news.json");
+	s = json.load(f)
+	f.close();
+	tid = s[newsType]
+	url = "http://c.m.163.com/nc/article/list/" + tid + "/" + str(page*10) + "-20.html"
+	return url
+	# return urllib2.urlopen(url).read()
